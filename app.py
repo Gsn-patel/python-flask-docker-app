2fe52427c1baff68c7954213c319349863e67d6c
Gsn-patel <152723098+Gsn-patel@users.noreply.github.com>
2024-01-27 17:11:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "your python application deployed succesfully in docker container..."
+    return "your python application dockerized successfully..."
 
 
 if __name__ == "__main__":
